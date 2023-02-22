d1b2ca349c10cce55b42b8565606aeeff1187f2f
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-02-22 16:49:21 +0100

    feat: add shaders

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three'
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
 import './style.css'
 
 // ____ RENDERER ____
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({ antialias: true })
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement)
@@ -10,13 +12,17 @@ document.body.appendChild(renderer.domElement)
 // ____ SCENE & CAMERA ____
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(40, 0, 0)
+camera.position.set(15, 0, 0)
 camera.lookAt(new THREE.Vector3(0, 0, 0))
 
 // ____ SHAPES ____
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
-  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('images/earth-uv-map.jpg') })
+  new THREE.ShaderMaterial({
+    // map: new THREE.TextureLoader().load('images/earth-uv-map.jpg')
+    vertexShader,
+    fragmentShader,
+  })
 )
 scene.add(sphere)
 
