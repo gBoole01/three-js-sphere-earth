2254fe8e23a96ba505633d5197a0909d671457f6
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-02-22 17:55:56 +0100

    feat: add atmoshpere

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
+import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
+import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
 import './style.css'
 
 // ____ RENDERER ____
@@ -12,7 +14,7 @@ document.body.appendChild(renderer.domElement)
 // ____ SCENE & CAMERA ____
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(15, 0, 0)
+camera.position.set(10, 0, 0)
 camera.lookAt(new THREE.Vector3(0, 0, 0))
 
 // ____ SHAPES ____
@@ -29,6 +31,20 @@ const sphere = new THREE.Mesh(
   })
 )
 scene.add(sphere)
+
+const atmosphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.ShaderMaterial({
+    vertexShader: atmosphereVertexShader,
+    fragmentShader: atmosphereFragmentShader,
+    blending: THREE.AdditiveBlending,
+    side: THREE.BackSide,
+  })
+)
+
+atmosphere.scale.set(1.1, 1.1, 1.1)
+scene.add(atmosphere)
+
 
 // ____ ANIMATIONS ____
 function animate() {
