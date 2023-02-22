cb5c4081da32e5941b3f9ea3f8d07421469e177a
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-02-22 18:43:43 +0100

    feat: change camera position

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ document.body.appendChild(renderer.domElement)
 // ____ SCENE & CAMERA ____
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.z = 10
+camera.position.z = 15
 camera.lookAt(new THREE.Vector3(0, 0, 0))
 
 // ____ SHAPES ____
