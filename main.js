0641b5d12c7163e173ba38431944e7c8d4a72f1c
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-02-22 14:36:40 +0100

    feat: add sphere and texture

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 import * as THREE from 'three'
 import './style.css'
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-
+// ____ RENDERER ____
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement)
 
+// ____ SCENE & CAMERA ____
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(40, 0, 0)
+camera.lookAt(new THREE.Vector3(0, 0, 0))
+
+// ____ SHAPES ____
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('images/earth-uv-map.jpg') })
+)
+scene.add(sphere)
+
+// ____ ANIMATIONS ____
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
