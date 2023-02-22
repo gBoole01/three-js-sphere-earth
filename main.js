5c39080e786aafea9b6d5b62132336aa5107ddcf
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-02-22 18:38:59 +0100

    feat: add animation, mousetracking, and stars

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
 import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
@@ -14,7 +15,7 @@ document.body.appendChild(renderer.domElement)
 // ____ SCENE & CAMERA ____
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.set(10, 0, 0)
+camera.position.z = 10
 camera.lookAt(new THREE.Vector3(0, 0, 0))
 
 // ____ SHAPES ____
@@ -30,7 +31,6 @@ const sphere = new THREE.Mesh(
     }
   })
 )
-scene.add(sphere)
 
 const atmosphere = new THREE.Mesh(
   new THREE.SphereGeometry(5, 50, 50),
@@ -45,14 +45,51 @@ const atmosphere = new THREE.Mesh(
 atmosphere.scale.set(1.4, 1.4, 1.4)
 scene.add(atmosphere)
 
+// ____ GROUP ____
+const group = new THREE.Group()
+group.add(sphere)
+scene.add(group)
+
+// ____ STARS ____
+const starGeometry = new THREE.BufferGeometry()
+const starMaterial = new THREE.PointsMaterial({
+  color: 0xffffff,
+})
+
+const starVertices = []
+for (let i = 0; i < 10000; i++) {
+  const x = (Math.random() - 0.5) * 2000
+  const y = (Math.random() - 0.5) * 2000
+  const z = -Math.random() * 2000
+  starVertices.push(x, y, z)
+}
+starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3))
+
+const stars = new THREE.Points(starGeometry, starMaterial)
+scene.add(stars)
 
 // ____ ANIMATIONS ____
+const mouse = {
+  x: undefined,
+  y: undefined,
+}
 function animate() {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
-  sphere.rotation.y += 0.001
+  sphere.rotation.y += 0.003
+  gsap.to(group.rotation, {
+    x: -mouse.y * 0.3,
+    y: mouse.x * 0.5,
+    duration: 1,
+  })
 }
 animate()
+
+// ____ EVENTS ____
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+})
 
 addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
